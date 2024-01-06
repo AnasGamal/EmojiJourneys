adaaca513668747e8a1775cb0f77b5a8a366ba71
AnasGamal <agamal@stetson.edu>
2024-01-05 23:57:24 -0500

    Initial Commit

---
+++ EmojiJourneys/EmojiJourneysApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiJourneysApp.swift
+//  EmojiJourneys
+//
+//  Created by Anas Gamal on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiJourneysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
